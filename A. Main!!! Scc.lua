a1961ed9cf7c1ae303ff856044340177d1e3716b
Mr. Supakrid boonyong <supakridboonyong@gmail.com>
2025-06-10 09:06:00 +0700

    A. Main!!! Scc.lua

--- A. Main!!! Scc.lua
+++ A. Main!!! Scc.lua
@@ -1,14 +1,14 @@
 for e = 1,6 do
 	
-local RN = math.random(1,10)
+local RN = math.random(3,6)
 print("Top Num "..RN)
 local OutputLabel = script.Parent:WaitForChild("ScreenGui"):WaitForChild("Label" .. e):WaitForChild("TL" .. e)
 
 	OutputLabel.Text = RN
 if  1 <= RN and RN <= 100 then
 	print("True")
-	local loop = math.random(3,6)
-	print("Cycle: ".. loop)
+	--local loop = math.random(3,6)
+	print("Cycle: ".. RN)
 	
 	local B = {
 		" ",
@@ -18,7 +18,7 @@ if  1 <= RN and RN <= 100 then
 		" ",
 		" ",	
 	}
-	for i = 1,loop do
+	for i = 1,RN do
 		local Num2 = math.random(-99,99)
 			local label = script.Parent:WaitForChild("ScreenGui"):WaitForChild("Label" .. e)
 			local TL = label:WaitForChild("TL" .. e)
@@ -28,7 +28,7 @@ if  1 <= RN and RN <= 100 then
 		B[i] = vAlue
 			print(Num2)
 			wait(0.2)
-		if i == loop then
+		if i == RN then
 			local labelScr = script.Parent:WaitForChild("ScreenGui"):WaitForChild("Label" .. e)
 			labelScr.Text = B[1] .."  " .. B[2] .. "  " .. B[3] .. "  " .. B[4] .. "  " .. B[5] .. "  " .. B[6]
 			print("Scr")
